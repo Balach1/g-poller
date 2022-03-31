daf5e52fb3af8b5bf840082914ce29ab8996e5bf
Balach Subra <balachsubra@gmail.com>
2022-03-31 17:51:39 +0100

    added to cypress env

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,29 +15,41 @@
 const debug = require("debug");
 const path = require("path");
 const gmail_tester = require("../../node_modules/gmail-tester");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on) => {
+require("dotenv").config();
+
+module.exports = (on, config) => {
+  // modify env value
+  console.log(config);
+  // return config
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on("task", {
-    "gmail:get-messages": async args => {
+    "gmail:get-messages": async (args) => {
+      config.env = config.env || {};
+      // you could extract only specific variables
+      // and rename them if necessary
+      let con = config.env.credentials;
+      con = process.env.credentials;
+      let con2 = config.env.gmail_token;
+      con2 = process.env.gmail_token;
+      
       const messages = await gmail_tester.get_messages(
-        path.resolve(__dirname, "credentials.json") || Cypress.env('credentials'),
-        path.resolve(__dirname, "gmail_token.json") || Cypress.env('gmail_token'),
+        path.resolve(__dirname, "../../", "credentials.json"),
+        path.resolve(__dirname, "../../", "gmail_token.json"),
+        // path.resolve(Cypress.env("credentials")),
+        // path.resolve(Cypress.env("gmail_token")),
         args.options
       );
-      return messages;
-    }
-  });
-};
 
-// module.exports = (on) => {
-//   on('task', {
-//     'seedRisks' () {
-//        console.log('sdjdsj'); //just a placeholder
-//      }
-//   })
-// }
+      console.log("------------", con);
+      console.log("------------", con2);
+      return messages;
+    },
+  });
+  return config;
+};
